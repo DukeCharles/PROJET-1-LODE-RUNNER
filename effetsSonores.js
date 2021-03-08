b5dac22e04f4f61a286462f94f4d21ca2fbc1482
Ab <djandrewab@outlook.com>
2021-03-08 15:41:45 -0500

    initSons de effetsSonores.js

--- effetsSonores.js
+++ effetsSonores.js
@@ -1,3 +1,49 @@
-function jouerSon(snd) {
-    snd.play();
+function initSons() {
+    objSons = new Object();
+
+    let objSon = document.createElement('audio');
+    objSon.setAttribute('src', 'tombe.mp3');
+    objSon.load();
+    objSons.tombe = objSon;
+
+    objSon = document.createElement('audio');
+    objSon.setAttribute('src', 'detruit.mp3');
+    objSon.load();
+    objSons.detruit = objSon;
+
+    objSon = document.createElement('audio');
+    objSon.setAttribute('src', 'auto_remplit_trou.mp3');
+    objSon.load();
+    objSons.auto_remplit_trou = objSon;
+
+    objSon = document.createElement('audio');
+    objSon.setAttribute('src', 'gameover.mp3');
+    objSon.load();
+    objSons.gameover = objSon;
+
+    objSon = document.createElement('audio');
+    objSon.setAttribute('src', 'gardeMeurt.mp3');
+    objSon.load();
+    objSons.gardeMeurt = objSon;
+
+    objSon = document.createElement('audio');
+    objSon.setAttribute('src', 'gardeTombeTrou.mp3');
+    objSon.load();
+    objSons.gardeTombeTrou = objSon;
+
+    objSon = document.createElement('audio');
+    objSon.setAttribute('src', 'loderunnerHit.mp3');
+    objSon.load();
+    objSons.loderunnerHit = objSon;
+
+    objSon = document.createElement('audio');
+    objSon.setAttribute('src', 'nextLevel.mp3');
+    objSon.load();
+    objSons.nextLevel = objSon;
+
+    objSon = document.createElement('audio');
+    objSon.setAttribute('src', 'ramasserOr.mp3');
+    objSon.load();
+    objSons.ramasserOr = objSon;
+
 }
