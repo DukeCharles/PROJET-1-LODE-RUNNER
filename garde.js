a32361255366be9d228420a2f406dfe5550d1689
Chuck <charles.simon.morin@gmail.com>
2021-03-09 16:36:07 -0500

    Garde Collision et GameOver

--- garde.js
+++ garde.js
@@ -60,9 +60,16 @@
         return tabEmplacementGardes[Math.floor(Math.random() * Math.floor(tabEmplacementGardes.length - 1))];
     }
 
+	/**
+	 * 
+	 * @param {ImageData} imgData Les données de l'image en pixel à recolorier
+	 * @param {Object} garde L'objet du garde correspondant
+	 * @returns l'imageData ayant subi des modifications par rapport à sa couleur.
+	 */
 	function recolorGardeImage(imgData, garde) {
 			for(let p = 0; p < imgData.data.length; p += 4) {
 
+				//Changer la couleur du chandail du garde
 				if(imgData.data[p] >= 45 && imgData.data[p] <= 55
 				&& imgData.data[p+1] >= 70 && imgData.data[p+1] <= 80
 				&& imgData.data[p+2] >= 160 && imgData.data[p+2] <= 170) {
@@ -70,7 +77,24 @@
 					imgData.data[p+1] = garde.GREEN;
 					imgData.data[p+2] = garde.BLUE;
 				}
+				//Regarde si le garde est en possèssion de coffre d'or
+				if(garde.nCoffre > 0) {
+					//Si oui recolorié les pantalons de façons doré
+				}
+
 			}
 
 		return imgData;
 	}
+
+	/**Regarde si le garde est coincer dans un trou
+	 * @returns si le garde est coincé ou non
+	 */
+	function estCoincer() {
+		//Condition nécèssaire
+
+		//Est sur un bloc qui est une brique
+		//N'a aucune action possible à vrai?
+		return false;
+	}
+
